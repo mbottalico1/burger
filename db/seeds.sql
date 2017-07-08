5727c3b606b63b054209f4d7118c2b5162297034
Michael Bottalico <yankeefan72193@aol.com>
2017-07-08 18:30:53 -0400

    updated

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name,devoured, date) VALUES('Home-cooked Burger', 1, '2015-05-08 08:30:02');
+
+INSERT INTO burgers(burger_name,devoured, date) VALUES('Jr Bacon Cheese Burger', 0, '2017-01-15 12:04:52');
--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use("/", routes);
+
+app.listen(port);
+
+module.exports = router;
