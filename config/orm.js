133374d98f11a9ecab43e3f854d4103aef00301b
Michael Bottalico <yankeefan72193@aol.com>
2017-07-10 12:41:16 -0400

    updating

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,9 @@
 var connection = require('../config/connections.js');
+
+connection.connect(function(err) {
+  if (err) {
+    throw err;
+  };
+  console.log('connected as id ' + connection.threadId);
+});
+
--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controllers.js");
+var router = require("./controllers/burgers_controllers.js");
 
-app.use("/", routes);
+app.use("/", router);
 
 app.listen(port);
 
