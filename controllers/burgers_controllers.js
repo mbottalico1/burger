c1a0c65560356fd550cdd2d19534bc7fe1575a7b
Michael Bottalico <yankeefan72193@aol.com>
2017-07-12 16:46:18 -0400

    updating

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -14,7 +14,7 @@ router.get('/burgers', function(req, res) {
 });
 
 router.post('/burgers/create', function(req, res) {
-	burgers.create([ 'burger_name'], [req.body.burger_name ], function (data) {
+	burgers.create(['burger_name'], [req.body.burgerName], function (data) {
 		res.redirect('/');
 	})
 	 
